8416b8d4fa42136468ad4c7dbe3766fb4d73922c
pwambui2020 <paulynewambui2020@gmail.com>
2020-09-01 13:51:39 +0300

    add all the hover elements and onclick elements

--- js/main.js
+++ js/main.js
@@ -16,4 +16,45 @@ $(document).ready(function(){
         console.log('any');
       $("#planning-div").toggle(1000);
     });
-  });
+ $('#port1').hover(function(){
+     $('.porttext').toggle();
+ })
+});
+
+$(document).ready(function(){
+  $("#port1").hover(function(){
+  document.getElementById('porttext1').style.zIndex="1";
+  })
+  
+  $("#port2").hover(function(){
+  document.getElementById('porttext2').style.zIndex="1";
+  })
+
+
+  $("#port3").hover(function(){
+  document.getElementById('porttext3').style.zIndex="1";
+  })
+
+
+  $("#port4").hover(function(){
+  document.getElementById('porttext4').style.zIndex="1";
+  })
+
+
+  $("#port5").hover(function(){
+  document.getElementById('porttext5').style.zIndex="1";
+  })
+
+
+  $("#port6").hover(function(){
+  document.getElementById('porttext6').style.zIndex="1";
+  })
+
+  $("#port7").hover(function(){
+    document.getElementById('porttext7').style.zIndex="1";
+    })
+
+    $("#port8").hover(function(){
+      document.getElementById('porttext8').style.zIndex="1";
+      })
+});
